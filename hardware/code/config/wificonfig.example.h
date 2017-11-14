2bda0f35fee0408815ad742c6c783b19b919fad1
Frank Hessel <frank@fhessel.de>
2017-11-14 11:35:09 +0100

    Some git reconfiguration

--- hardware/code/config/wificonfig.example.h
+++ hardware/code/config/wificonfig.example.h
@@ -8,6 +8,13 @@
 #ifndef CONFIG_WIFICONFIG_H_
 #define CONFIG_WIFICONFIG_H_
 
+/*
+ *
+ * - Rename this file to wificonfig.h
+ * - Insert your wifi ssid and psk into the copied file
+ *
+ */
+
 const char* WIFI_SSID = (const char*)"<insert ssid here>";
 const char* WIFI_PSK  = (const char*)"<insert pre shared key here>";
 
