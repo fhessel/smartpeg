53df0435a7ed8131256fefa04ba34f169d9d530e
Frank Hessel <frank@fhessel.de>
2017-12-22 20:02:39 +0100

    Enabled training data generation

--- api/rest/src/main/java/de/tudarmstadt/smartpeg/scheduler/ScheduledExecutor.java
+++ api/rest/src/main/java/de/tudarmstadt/smartpeg/scheduler/ScheduledExecutor.java
@@ -34,7 +34,7 @@ public class ScheduledExecutor implements ServletContextListener {
 	    
 	    // The DataExtractionTask creates training samples.
 	    // As it is a time consuming task, we schedule it only every hours hours, so three times a day
-	    //executor.scheduleAtFixedRate(new DataExtractionTask(), 0, 8, TimeUnit.HOURS);
+	    executor.scheduleAtFixedRate(new DataExtractionTask(), 0, 8, TimeUnit.HOURS);
 	    
 	    logger.info("Scheduler has been started");
 	}
